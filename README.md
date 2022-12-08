68ef6497d48cef7375b98a2b29454d19fdc8e0ab
BenstBen <90843097+BenstBen@users.noreply.github.com>
2022-12-08 09:38:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-duep6u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-duep6u)
---
+++ schmuck.ts
@@ -0,0 +1,14 @@
+import { Gegenstand } from "./gegenstand"; //von Stackblitz automat. generiert
+
+export class Schmuck extends Gegenstand {
+
+  constructor(id: number, wert: number, public bezeichnung: string) {
+	super(id,wert);
+  }
+  
+  toString(): string {
+    let text: string = super.toString();
+    text += "\nbezeichnung" + this.bezeichnung;
+    return text;
+
+} }
